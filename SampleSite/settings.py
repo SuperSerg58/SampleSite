aa1a6a540a8402542cb7c0a7e8551de2629f9774
Sergey Alexeev <demiurg58@gmail.com>
2020-11-18 09:44:42 +0300

    -

--- SampleSite/settings.py
+++ SampleSite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = None
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
